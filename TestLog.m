c1ff991638734749244553d04c05c6eb5e9be60d
郑东喜 <19259129536@163.com>
2019-08-23 17:00:28 +0800

    添加TestLog文件

---
+++ TestLog.m
@@ -0,0 +1,16 @@
+//
+//  TestLog.m
+//  Pods-ZDXTestDemo_Tests
+//
+//  Created by 郑东喜 on 2019/8/23.
+//
+
+#import "TestLog.h"
+
+@implementation TestLog
+
++ (void)testLog {
+    NSLog(@"====");
+}
+
+@end
